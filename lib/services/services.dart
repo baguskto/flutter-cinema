a4f439e726a563c27d84bccdd31f57116167a4b9
baguskto <baguskto@gmail.com>
2021-02-27 14:36:28 +0700

    update firebase

---
+++ lib/services/services.dart
@@ -0,0 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutix/extensions/extensions.dart';
+import 'package:flutix/models/models.dart';
+part 'auth_services.dart';
+part 'user_services.dart';
