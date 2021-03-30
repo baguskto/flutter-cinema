d350a883c5ffd5b84ad5b10833dbe2d1e36de62e
baguskto <baguskto@gmail.com>
2021-03-30 10:03:22 +0700

    feat: implement account confirmation state

--- lib/ui/pages/wrapper.dart
+++ lib/ui/pages/wrapper.dart
@@ -27,6 +27,8 @@ class Wrapper extends StatelessWidget {
                     ? SignUpPage(pageState.registrationData)
                     : (pageState is OnPreferencePage)
                         ? PreferencePage(pageState.registrationData)
-                        : MainPage());
+                        : (pageState is OnAccountConfirmationPage)
+                            ? AccountConfirmationPage(pageState.registrationData)
+                            : MainPage());
   }
 }
